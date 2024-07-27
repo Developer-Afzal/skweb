e1a83626536d34db5a6ff5129ced8ef56ea9b21c
Developer-Afzal <afzalansari891@gmail.com>
2024-07-27 11:32:09 +0530

    update the function

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import { GET, POST } from '../utils/Api'
 const Success = () => {
     const {id} = useParams()
-    const [paymantData, setpaymantData] = useState(second)
+    const [paymantData, setpaymantData] = useState(false)
     useEffect(()=>{
        const FetchInfo = async ()=>{
         const Response = await GET(`success/${id}`)
